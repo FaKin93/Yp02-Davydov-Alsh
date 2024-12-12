61e1f0329eb9eb17df4b8f44b05bdd9a5375e992
Владислав Давыдов <vlad5803964@yandex.ru>
2024-12-12 16:27:58 +0300

    Добавьте файлы проекта.

---
+++ Yp02-Davydov-Alsh/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Yp02_Davydov_Alsh
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Partners currentPartners = new Partners();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var context = Entities.GetContext();
+            var currentPartners = context.Partners.ToList();
+            ListPartners.ItemsSource = currentPartners;
+            UpdatePartners();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены что хотите закрыть приложение?", "Подтвержение закрытия", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+                e.Cancel = true;
+        }
+        private void UpdatePartners()
+        {
+            //загружаем всех пользователей в список
+            var currentUsers = Entities.GetContext().Partners.ToList();
+        }
+    }
+}
